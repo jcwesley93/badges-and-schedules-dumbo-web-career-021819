4f54867c0319244b70476dc66bc71132100935b6
Jaela Wesley <jcwesley93@gmail.com>
2019-02-05 10:11:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,8 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  batch_badge_creator(array). each do |badge|
+  batch_badge_creator(array).each do |badge|
     puts badge
   end
+  assign_rooms(array)
 end
