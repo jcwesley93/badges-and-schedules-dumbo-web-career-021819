0ddc3c586e41849178ec4868679d67588151e2f2
Jaela Wesley <jcwesley93@gmail.com>
2019-02-05 09:42:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-badges = [
+badges = []
   array.each do |name|
-   return "Hello, my name is #{name}."
+    "Hello, my name is #{name}."
+    badges << name
   end
-]
 end
